8eb551e4fff4d773a4bb1c70174aaf44bf770b26
jeonggeonwoong <ghtm543@gmail.com>
2023-06-12 16:32:17 +0900

    update sequelize config

--- config/config.js
+++ config/config.js
@@ -1,32 +1,28 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-exports.default = {
-    development: {
-        username: 'geonwoong',
-        password: process.env.DB_PASSWORD,
-        database: 'realworld-api',
-        host: '127.0.0.1',
-        dialect: 'mysql',
-        logging: true,
-    },
-    test: {
-        username: "geonwoong",
-        password: process.env.DB_PASSWORD,
-        database: 'realworld-api',
-        host: "127.0.0.1",
-        dialect: "mysql"
-    },
-    production: {
-        username: 'geonwoong',
-        password: process.env.DB_PASSWORD,
-        database: 'realworld-api',
-        host: '127.0.0.1',
-        dialect: 'mysql',
-        logging: false,
-    },
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.USER_NAME,
+    password: process.env.SEQUELIZE_PWD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: 'mysql',
+    logging: true,
+  },
+  test: {
+    username: process.env.USER_NAME,
+    password: process.env.SEQUELIZE_PWD,
+    database: process.env.DATABASE,
+    host: "127.0.0.1",
+    dialect: "mysql",
+    logging: true,
+  },
+  production: {
+    username: process.env.USER_NAME,
+    password: process.env.SEQUELIZE_PWD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
+    dialect: 'mysql',
+    logging: false,
+  },
 };
